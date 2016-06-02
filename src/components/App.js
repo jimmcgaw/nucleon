92ce9167ce971cea44984b2d18b559431f9ca0b9
jimmcgaw <jpmcgaw@gmail.com>
2016-06-02 15:55:09 -0700

    header component with some styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render(){
     return (
       <div className="container-fluid">
+        <Header />
         <p>Hormones for Plants</p>
         {this.props.children}
       </div>
